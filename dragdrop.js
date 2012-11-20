e401f7e957b448cfd79a9c0ccfd424321b29970b
Jean-Baptiste Demonte <jbdemonte@gmail.com>
2012-11-20 23:26:09 +0100

    append new callback

--- dragdrop.js
+++ dragdrop.js
@@ -1,11 +1,11 @@
 /*!
  *  Drag and drop plugin for jQuery 
- *  Version   : 1.0
- *  Date      : 2012-11-13
- *  Licence   : MIT  
+ *  Version   : 1.1
+ *  Date      : 2012-11-20
+ *  Licence   : MIT
  *  Author    : DEMONTE Jean-Baptiste
  *  Contact   : jbdemonte@gmail.com
- *   
+ *  
  *  Copyright (c) 2010-2012 Jean-Baptiste DEMONTE
  *  All rights reserved.
  */
@@ -21,29 +21,51 @@
                 } else {
                     value = data;
                 }
+                event.originalEvent.dataTransfer.effectAllowed = 'move';
+                event.originalEvent.dataTransfer.setData('Text', this.id || "");
+                event.originalEvent.dataTransfer.setDragImage(event.target, 0, 0);
             });
         });
         return this;
     };
-    $.fn.drop = function (hover, drop) {
-        if (typeof drop === "undefined") {
-            drop = hover;
-            hover = null;
+    $.fn.drop = function (fncs) {
+        if (typeof fncs === "function") {
+            fncs = {drop: fncs};
         }
         this.each(function () {
             var item = $(this);
+            if (fncs.dragstart) {
+                $(document).bind("dragstart", function (event) {
+                    fncs.dragstart.call(item, event, value);
+                });
+            }
+            if (fncs.dragend) {
+                $(document).bind("dragend", function (event) {
+                    fncs.dragend.call(item, event, value);
+                });
+            }
+            if (fncs.enter) {
+                item.on("dragenter", function (event) {
+                    return fncs.enter.call(item, event, value);
+                });
+            }
             item.on("dragover", function (event) {
-                if (typeof hover === "function") {
-                    hover.call(item, event, value);
+                if (fncs.hover) {
+                    fncs.hover.call(item, event, value);
                 } else {
                     event.preventDefault();
                 }
             });
-            if (typeof drop === "function") {
-                item.on("drop", function (event) {
-                    drop.call(item, event, value);
+            if (fncs.leave) {
+                item.on("dragleave", function (event) {
+                    return fncs.leave.call(item, event, value);
                 });
             }
+            item.on("drop", function (event) {
+                if (fncs.drop) {
+                    fncs.drop.call(item, event, value);
+                }
+            });
         });
         return this;
     };
